1bead8098327c244f80cdb4739f65067fa183682
KibobiShtrudelz <petar.georgiev.kolev@gmail.com>
2023-12-13 18:14:58 +0200

    init project

---
+++ src/app/app.tsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+export function App() {
+  return (
+    <div>
+      <h1>WAZA</h1>
+    </div>
+  );
+}
