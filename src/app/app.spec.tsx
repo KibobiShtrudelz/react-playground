c990ac820ce9a5ebd426f9cb67cd804c3cbfd9e5
KibobiShtrudelz <petar.georgiev.kolev@gmail.com>
2024-01-10 00:20:23 +0200

    init project

--- src/app/app.spec.tsx
+++ src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />)
-    expect(getByText(/Welcome react-playground/gi)).toBeTruthy()
+    expect(getByText(/Welcome template/gi)).toBeTruthy()
   })
 })
